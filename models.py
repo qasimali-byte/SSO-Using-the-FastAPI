69def5c295929526956c4728ea18c5401c4281f1
syedfaisalsaleeem <faisal.saleem@attech-ltd.com>
2022-04-18 01:26:04 +0500

    adding docker for deployment

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String(100),nullable=False, unique=True )
+    email = Column(String(100),nullable=False, unique=True)
     password = Column(String(100), nullable=False)
     username = Column(String(1000))
 
