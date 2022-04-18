9bf9092a147de03d8f673913ca51448343e87c05
syedfaisalsaleeem <faisal.saleem@attech-ltd.com>
2022-04-18 16:01:08 +0500

    initializa database

--- init-database.sh
+++ init-database.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-
+echo -n "blah"
 psql -v ON_ERROR_STOP=1 --username postgres --password faisal --dbname sso_idp <<-EOSQL
     CREATE USER postgres With PASSWORD 'faisal';
     CREATE DATABASE sso_idp;
