8a3e8d1b9e2732bc61e812efd329c2d41e38f985
syedfaisalsaleeem <faisal.saleem@attech-ltd.com>
2022-04-18 16:56:55 +0500

    running on port 80

--- core/config.py
+++ core/config.py
@@ -14,7 +14,7 @@ class Settings:
     POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD","faisal")
     POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "host.docker.internal")
     POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
-    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "sso_idp")
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "postgres")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
     # SECRET_KEY: str = os.getenv("SECRET_KEY")
--- main.py
+++ main.py
@@ -325,4 +325,4 @@ async def logout(request: Request,response : Response,SAMLRequest: str,db: Sessi
 #   return response
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
