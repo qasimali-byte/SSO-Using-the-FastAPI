14476cb9b4c3d5da3c0f2b926e4a8ff2b8de7269
syedfaisalsaleeem <faisal.saleem@attech-ltd.com>
2022-05-19 01:10:58 +0500

    migrations task completed

--- src/apis/v1/models/serviceprovidersmodel.py
+++ src/apis/v1/models/serviceprovidersmodel.py
@@ -5,3 +5,4 @@ class Sps(Base):
     __tablename__ = "service_providers"
     id = Column(Integer, primary_key=True, index=True)
     sp_id = Column(String(100),nullable=False, unique=True )
+    sp_name = Column(String(100),nullable=True, unique=False, default="")
