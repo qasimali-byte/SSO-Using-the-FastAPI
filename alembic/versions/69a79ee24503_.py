9f55c340f5226a399b51a95ac169bb6705ccb49a
syedfaisalsaleeem <faisal.saleem@attech-ltd.com>
2022-05-18 14:34:50 +0500

    adding migrations work

---
+++ alembic/versions/69a79ee24503_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 69a79ee24503
+Revises: 
+Create Date: 2022-05-18 12:48:59.990244
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '69a79ee24503'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- load_env.py
+++ load_env.py
@@ -6,8 +6,8 @@ parser = argparse.ArgumentParser(description="IDP Server Arguments",
                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-e","--env", help="Select the environment to run the server in",
                     type=str,required=False,choices=['dev', 'prod'])
-args = parser.parse_args()
-config = vars(args)
+# args = parser.parse_args()
+config = {'env': 'dev'}
 
 if config['env'] == 'dev':
     env_path = Path(".") / "envs/.env.dev"
