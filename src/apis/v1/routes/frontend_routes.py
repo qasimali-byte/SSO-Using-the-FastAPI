b13e7afb4de736cd64fc2eb659f2b1056610a5c9
syedfaisalsaleeem <faisal.saleem@attech-ltd.com>
2022-05-31 12:53:04 +0500

    serving html

--- src/apis/v1/routes/frontend_routes.py
+++ src/apis/v1/routes/frontend_routes.py
@@ -6,6 +6,9 @@ templates = Jinja2Templates(directory="build")
 router = APIRouter(tags=["SSO Frontend"])
 
 @router.get("/")
+async def serve_spa(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
 @router.get("/sign-in")
 async def serve_spa(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
