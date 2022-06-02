a97970bed76c34129f3282a03247acb5f086b7c5
syedfaisalsaleeem <faisal.saleem@attech-ltd.com>
2022-06-02 12:47:05 +0500

    list service providers api

--- load_env.py
+++ load_env.py
@@ -6,9 +6,9 @@ parser = argparse.ArgumentParser(description="IDP Server Arguments",
                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-e","--env", help="Select the environment to run the server in",
                     type=str,required=False,choices=['dev', 'prod'])
-# args = parser.parse_args()
-# config = vars(args)
-config = {'env': 'dev'}
+args = parser.parse_args()
+config = vars(args)
+# config = {'env': 'dev'}
 
 if config['env'] == 'dev':
     env_path = Path(".") / "envs/.env.dev"
