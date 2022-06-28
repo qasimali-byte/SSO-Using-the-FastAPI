25057d60de20584e1a3fd1fea96094649113f548
qasim <qasim.ali@attech-ltd.com>
2022-06-28 13:00:11 +0500

    get users data and their apps rights 28-06-2022

--- src/apis/v1/controllers/users_controller.py
+++ src/apis/v1/controllers/users_controller.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from this import s
+from src.apis.v1.helpers.custom_exceptions import CustomException
 from src.apis.v1.helpers.customize_response import custom_response
 from fastapi import status
 from src.apis.v1.validators.common_validators import ErrorResponseValidator
@@ -15,7 +17,5 @@ class UsersController():
             response = custom_response(data=data,status_code=status.HTTP_200_OK)
             return response
         except Exception as e:
-            data = ErrorResponseValidator(message="Some thing went wrong",status=False)
-            response = custom_response(data=data,status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
-            return response
+            raise CustomException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, message=str(e)+"- error occured in user_controller.py")
         
