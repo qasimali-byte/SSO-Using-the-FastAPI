48e85c33d8662526eaa98720a4f3fd50f95e2c85
syedfaisalsaleeem <syedfaisalsaleem.100@gmail.com>
2022-07-25 13:21:29 +0500

    adding redis to send email worker

--- src/apis/v1/workers/worker.py
+++ src/apis/v1/workers/worker.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 from utility import send_email
 from celery import Celery
 celery = Celery(__name__)
-celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
-celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
+celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://redis:6379")
+celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://redis:6379")
 load_dotenv(dotenv_path=".env.worker")
 
 
