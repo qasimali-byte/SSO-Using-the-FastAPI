9def0ef287e3e65a6a12090c49f2ab6b6d33e007
syedfaisalsaleeem <syedfaisalsaleem.100@gmail.com>
2022-07-26 14:27:59 +0500

    adding delete cookie functionality

--- src/apis/v1/helpers/global_helpers.py
+++ src/apis/v1/helpers/global_helpers.py
@@ -5,9 +5,21 @@ def delete_all_cookies(response, only_frontend=False):
     if only_frontend:
         cookie_frontend.delete_from_response(response)
         return
+    try:
+        response.delete_cookie("cookie_frontend")
+    except:
+        print("error deleting cookies")
+    try:
+        response.delete_cookie("cookie_idp")
+    except:
+        print("error deleting cookies")
+    try:
+        response.delete_cookie("cookie")
+    except:
+        print("error deleting cookies")
 
-    cookie.delete_from_response(response)
-    cookie_frontend.delete_from_response(response)
+    # cookie.delete_from_response(response)
+    # cookie_frontend.delete_from_response(response)
 
 def create_unique_id():
     import uuid
