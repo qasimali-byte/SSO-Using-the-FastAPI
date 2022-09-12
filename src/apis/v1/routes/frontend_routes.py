52cf36d1bb55bb37a8985d9f53b85bb9f7039619
syedfaisalsaleeem <syedfaisalsaleem.100@gmail.com>
2022-09-12 18:28:28 +0500

    redirection issue for notification

--- src/apis/v1/routes/frontend_routes.py
+++ src/apis/v1/routes/frontend_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 templates = Jinja2Templates(directory="build")
-
+templates2 = Jinja2Templates(directory="templates")
 
 router = APIRouter(tags=["SSO Frontend"], include_in_schema=False)
 
@@ -15,3 +15,8 @@ router = APIRouter(tags=["SSO Frontend"], include_in_schema=False)
 @router.get("/home")
 async def serve_spa(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@router.get("/backend/notification")
+@router.post("/backend/notification")
+async def serve_notification_page(request: Request):
+    return templates2.TemplateResponse("notification.html", {"request": request})
