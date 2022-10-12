470a0658102d612961cf4c92f5cf8908333f127f
Asad Bukhari <asadbukharee@gmail.com>
2022-10-12 13:06:48 +0500

    build fixed

---
+++ build/static/js/560.1a52c029.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksso_frontend=self.webpackChunksso_frontend||[]).push([[560],{4560:function(n,e,r){r.r(e),r.d(e,{default:function(){return c}});var t=r(13239),s=(0,r(93457).Z)("div")((function(n){n.theme;return{height:"100vh",display:"grid",placeItems:"center",width:"100%"}})),i=r(80184);var c=function(n){var e=n.children;return(0,i.jsxs)(s,{children:[(0,i.jsx)(t.Z,{color:"info"}),e]})}}}]);
+//# sourceMappingURL=560.1a52c029.chunk.js.map
