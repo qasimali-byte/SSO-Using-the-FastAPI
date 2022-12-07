7429c00245ff4533690c87c99487136b86cd5730
qasim-heavydev <qasim.ali@attech-ltd.com>
2022-12-07 17:37:41 +0500

    07-12-2022 3rd commit

--- src/middleware.py
+++ src/middleware.py
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 from src.apis.v1.middleware.middleware import AddActionMiddleWare, SecurityHeadersMiddleware
 from src.apis.v1.constants.origins_enum import origins
 from fastapi.middleware.cors import CORSMiddleware
 
-=======
-from src.apis.v1.middleware.middleware import AddActionMiddleWare
-from src.apis.v1.constants.origins_enum import origins
-from fastapi.middleware.cors import CORSMiddleware
->>>>>>> 3d6d1277ab41be7d23c06f1c549d506a5cff47f6
 
 
 
@@ -21,10 +15,7 @@ def registering_middleware(application):
         allow_methods=["GET", "POST", "HEAD", "OPTIONS", "PUT", "DELETE"],
         allow_headers=["Access-Control-Allow-Headers","Set-Cookie", 'Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
     )
-<<<<<<< HEAD
     
-=======
->>>>>>> 3d6d1277ab41be7d23c06f1c549d506a5cff47f6
     application.add_middleware(AddActionMiddleWare)
     application.add_middleware(SecurityHeadersMiddleware, csp=True)
     
